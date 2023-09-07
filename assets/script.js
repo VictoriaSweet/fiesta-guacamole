48bfa930a8f2aefcef91fd656064f1351f84b9f3
Victoria <sweet.victoria1218@gmail.com>
2023-09-07 17:04:53 -0500

    fixed past present and future in JS

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,17 @@
 // in the html.
 
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var currentTime = new Date();
-var currentHour = currentTime.getHours();
-const clock = currentTime.toLocaleString();
-
+var now = new Date();
+var currentHour = now.getHours();
+const clock = now.toLocaleTimeString();
+// var perspective = ["present" , "past" , "future"];
+// present = present class
 $(function () {
   const hourBlock = $("#hour-block");
   for (let index = 0; index < hours.length; index++) {
     const hour = hours[index];
     let ampm = "AM";
-    let time = `${hour}${ampm}`;
+    let time = `${hour}AM`;
     if (hour > 11) {
       ampm = "PM";
       time = `${hour - 12}${ampm}`;
@@ -22,14 +23,20 @@ $(function () {
       ampm = "PM";
       time = `${hour}${ampm}`;
     }
-    // if current time = hour then display present
-    let perspective = "present";
-    if (currentTime == hour) {
-      perspective = present;
+
+    let perspective = "past";
+
+    // are we in the present? yes? perspective = "present"
+    if (currentHour == hour) {
+      perspective = "present";
     }
 
-    // if current time > hour then display future
-    // if current time < hour then display past
+    // are we in the future? yes? perspective = "future"
+
+    if (currentHour < hour) {
+      perspective = "future";
+    }
+
     $(hourBlock).append(
       `<div id="hour-${hour}" class="row time-block ${perspective}">
         <div class="col-2 col-md-1 hour text-center py-3">${time}</div>
@@ -39,25 +46,39 @@ $(function () {
         </button>
       </div>`
     );
-    console.log (perspective)
+console.log(now) //inner text to make time appear on browser page - make div and then inner text.
+
+    // use jquery to find the div with the ID hour-${hour}....
+    //inside div there is the button, find the div with that ID and then find the child button using jquery
+    console.log($(`#hour-${hour}`));
+// find button in div and add event listener 
+
+// bring text back out of local storage like in highscores in challenge 4 
+    // second way - instead of appending hour block as a string, make a jquery object for each of the tags and then you would have the button since it was just created with create element -  when created with this varitation, you would have a "button" created
   }
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  // local storage.
+
+  //**HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // useful when saving the description in local storage?**
   //
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
+  // block by comparing the id to the current hour.
+
+  //**HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // current hour in 24-hour time?**
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // the values of the corresponding textarea elements.
+
+  //**HINT: How can the id
+  // attribute of each time-block be used to do this?**
   //
   // TODO: Add code to display the current date in the header of the page.
 });
