4222c42fbab3445271061f0fd1eaa3f5b3298a5c
Victoria <sweet.victoria1218@gmail.com>
2023-09-10 12:09:55 -0500

    finished save feature in JS

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var now = new Date();
 var currentHour = now.getHours();
-const scheduleInfos = ["wash the car", "", "", "", "", "", "", "", ""];
+let scheduleInfos = ["", "", "", "", "", "", "", "", ""];
+
+const key = "scheduleData";
+// call get item to see what is already in storage
+const localStorageSchedule = localStorage.getItem(key);
+// parse it from a string to an array
+if (localStorageSchedule != null) {
+  scheduleInfos = JSON.parse(localStorageSchedule);
+}
 const clock = document.getElementById("clock");
 clock.innerHTML = now;
 
@@ -24,12 +32,9 @@ $(function () {
 
     let perspective = "past";
 
-    // are we in the present? yes? perspective = "present"
     if (currentHour == hour) {
       perspective = "present";
     }
-
-    // are we in the future? yes? perspective = "future"
 
     if (currentHour < hour) {
       perspective = "future";
@@ -38,7 +43,7 @@ $(function () {
     $(hourBlock).append(
       `<div id="hour-${hour}" class="row time-block ${perspective}">
         <div class="col-2 col-md-1 hour text-center py-3">${time}</div>
-        <textarea class="col-8 col-md-10 description" rows="3">${scheduleInfo} </textarea>
+        <textarea class="col-8 col-md-10 description" rows="3">${scheduleInfo}</textarea>
         <button class="btn saveBtn col-2 col-md-1" aria-label="save">
           <i class="fas fa-save" aria-hidden="true"></i>
         </button>
@@ -47,35 +52,29 @@ $(function () {
 
     let button = $(`#hour-${hour} button`);
     button.on("click", function () {
-      alert("Handler for `click` called.");
-    })
+      saveUserData();
+    });
 
     function saveUserData() {
       const key = "scheduleData";
       // call get item to see what is already in storage
-      // parse it from a string to an array 
       const localStorageSchedule = localStorage.getItem(key);
-      let schedule = scheduleInfos;
-      schedule = JSON.parse(localStorageSchedule);
+      // parse it from a string to an array
+      if (localStorageSchedule != null) {
+        scheduleInfos = JSON.parse(localStorageSchedule);
+      }
+
       // get what user types into text area
-      // put it in the array 
+      let textarea = $(`#hour-${hour} textarea`);
+
+      // put it in the array
+      scheduleInfos[index] = textarea.val();
 
       //then set array back into local storage ( stringify)
-      const scheduleJSON = JSON.stringify(schedule);
+      const scheduleJSON = JSON.stringify(scheduleInfos);
       localStorage.setItem(key, scheduleJSON);
     }
-    saveUserData();
   }
-
-  //**HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?**
-
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements.
-
-  //**HINT: How can the id
-  // attribute of each time-block be used to do this?**
 });
-
